a0a54c504285c00be0742d73657b6f9544fe3535
Nistor Alexandru Marius <djthorr@gmail.com>
2023-05-27 01:46:41 +0300

    Release 1.0.1 Packagist Ready

--- TestClasses/bootstrapTestClasses.php
+++ TestClasses/bootstrapTestClasses.php
@@ -11,6 +11,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'GlobalMockClass.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'GlobalMockClass2.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'GlobalMockSingleton.php';
 
-if (PHP_VERSION_ID >= 81000) {
+
+if (PHP_VERSION_ID >= 81000) {//
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'GlobalMockEnum.php';
 }
